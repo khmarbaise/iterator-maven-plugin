79b7e0ed7c9a769b55ca4b5ae3d9268db6aeb3fa
Karl-Heinz Marbaise <info@soebes.de>
2013-01-19 16:49:12 +0100

    - Fixed #2   mavenAssemblyPluginTest is now working and checks   the existence of the created archive files.

--- src/it/mavenAssemblyPluginTest/verify.groovy
+++ src/it/mavenAssemblyPluginTest/verify.groovy
@@ -6,35 +6,50 @@ t = new IntegrationBase()
 
 
 def getProjectVersion() {
-	def pom = new XmlSlurper().parse(new File(basedir, 'pom.xml'))
-   
-	  def allPlugins = pom.build.plugins.plugin;
-   
-	  def configurationMavenPlugin = allPlugins.find {
-		  item -> item.groupId.equals("com.soebes.maven.plugins") && item.artifactId.equals("itexin-maven-plugin");
-	  }
-	  
-	  return configurationMavenPlugin.version;
+    def pom = new XmlSlurper().parse(new File(basedir, 'pom.xml'))
+
+    def allPlugins = pom.build.plugins.plugin;
+
+    def configurationMavenPlugin = allPlugins.find { item ->
+	item.groupId.equals("com.soebes.maven.plugins") && item.artifactId.equals("itexin-maven-plugin");
+    }
+
+    return configurationMavenPlugin.version;
 }
-   
+
 def projectVersion = getProjectVersion();
-   
+
 println "Project version: ${projectVersion}"
-   
+
 
 def buildLogFile = new File( basedir, "build.log");
 
 
+
 t.checkExistenceAndContentOfAFile(buildLogFile, [
-  '[INFO] --- itexin-maven-plugin:' +projectVersion + ':executor (default) @ basic-test ---',
-  '[INFO] ]] com.soebes.maven.plugins:maven-echo-plugin:0.1',
-  '[INFO] This is a message: eins',
-  '[INFO] ]] com.soebes.maven.plugins:maven-echo-plugin:0.1',
-  '[INFO] This is a message: zwei',
-  '[INFO] ]] com.soebes.maven.plugins:maven-echo-plugin:0.1',
-  '[INFO] This is a message: drei',
-  '[INFO] BUILD SUCCESS',
+    '[INFO] --- itexin-maven-plugin:' +projectVersion + ':executor (default) @ basic-test ---',
+    '[INFO] ]] org.apache.maven.plugins:maven-assembly-plugin:2.4',
+    '[INFO] Reading assembly descriptor: ' + basedir + '/test.xml',
+    '[INFO] Building war: ' + basedir + '/target/mavenAssemblyTest-0.1-SNAPSHOT-test.war',
+    '[INFO] Reading assembly descriptor: ' + basedir + '/dev.xml',
+    '[INFO] Building war: ' + basedir + '/target/mavenAssemblyTest-0.1-SNAPSHOT-dev.war',
+    '[INFO] Reading assembly descriptor: ' + basedir + '/prod.xml',
+    '[INFO] Building war: ' + basedir + '/target/mavenAssemblyTest-0.1-SNAPSHOT-production.war',
+    '[INFO] BUILD SUCCESS',
 ])
+
+def test_war = new File (basedir, "/target/mavenAssemblyTest-0.1-SNAPSHOT-test.war");
+if (!test_war.exists()) {
+    throw new FileNotFoundException("The file " + test_war + " does not exist!");
+}
+def dev_war = new File (basedir, "/target/mavenAssemblyTest-0.1-SNAPSHOT-dev.war");
+if (!dev_war.exists()) {
+    throw new FileNotFoundException("The file " + dev_war + " does not exist!");
+}
+def prod_war = new File (basedir, "/target/mavenAssemblyTest-0.1-SNAPSHOT-production.war");
+if (!prod_war.exists()) {
+    throw new FileNotFoundException("The file " + prod_war + " does not exist!");
+}
 
 
 return true;
