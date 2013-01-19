d24546e16f1fe96a27a5ba2bc84731c206a95b64
Karl-Heinz Marbaise <info@soebes.de>
2013-01-19 21:39:08 +0100

    - Added integration test for content usage.

--- src/test/java/com/soebes/maven/plugins/itexin/ExecutorTest.java
+++ src/test/java/com/soebes/maven/plugins/itexin/ExecutorTest.java
@@ -7,22 +7,21 @@ import org.testng.annotations.Test;
 
 public class ExecutorTest extends UnitTestBase {
 
-    
     @Test
     public void first() {
 	List<String> items = new ArrayList<String>();
-	
+
 	items.add("one");
 	items.add("two");
 	items.add("three");
 
     }
-    
+
     @Test
     public void secondTest() {
 	List<String> results = new ArrayList<String>();
 	String items = "one, two, three";
-	
+
 	String[] result = items.split(",");
 	for (String string : result) {
 	    results.add(string.trim());
