783aa25ea83d97ffe4598f8625f7718f4059ea35
Florian Fray <florian.fray@itcf.biz>
2013-06-30 12:59:18 +0200

    Added test case for fixing khmarbaise/iterator-maven-plugin#4.

---
+++ src/it/itemsFromCLITest/verify.groovy
@@ -0,0 +1,42 @@
+import java.io.*
+import java.util.*
+
+
+t = new IntegrationBase()
+
+
+def getProjectVersion() {
+	def pom = new XmlSlurper().parse(new File(basedir, 'pom.xml'))
+   
+	  def allPlugins = pom.build.plugins.plugin;
+   
+	  def configurationMavenPlugin = allPlugins.find {
+		  item -> item.groupId.equals("com.soebes.maven.plugins") && item.artifactId.equals("itexin-maven-plugin");
+	  }
+	  
+	  return configurationMavenPlugin.version;
+}
+   
+def projectVersion = getProjectVersion();
+   
+println "Project version: ${projectVersion}"
+   
+
+def buildLogFile = new File( basedir, "build.log");
+
+
+t.checkExistenceAndContentOfAFile(buildLogFile, [
+  '[INFO] --- itexin-maven-plugin:' +projectVersion + ':executor (default) @ basic-test ---',
+  '[INFO] ------ com.soebes.maven.plugins:maven-echo-plugin:0.1:echo',
+  '[INFO] This is a message: zwei',
+  '[INFO] ------ com.soebes.maven.plugins:maven-echo-plugin:0.1:echo',
+  '[INFO] This is a message: mal',
+  '[INFO] ------ com.soebes.maven.plugins:maven-echo-plugin:0.1:echo',
+  '[INFO] This is a message: macht',
+  '[INFO] ------ com.soebes.maven.plugins:maven-echo-plugin:0.1:echo',
+  '[INFO] This is a message: vier',
+  '[INFO] BUILD SUCCESS',
+])
+
+
+return true;
