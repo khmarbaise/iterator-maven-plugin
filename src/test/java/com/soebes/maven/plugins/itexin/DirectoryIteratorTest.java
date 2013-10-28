a3ea72cd32133041239ca9faeffa82f066a078c1
Karl Heinz Marbaise <info@soebes.de>
2013-10-28 22:34:25 +0100

    - Continued directory iteration.

--- src/test/java/com/soebes/maven/plugins/itexin/DirectoryIteratorTest.java
+++ src/test/java/com/soebes/maven/plugins/itexin/DirectoryIteratorTest.java
@@ -5,7 +5,10 @@ import static org.fest.assertions.Assertions.assertThat;
 import java.io.File;
 import java.io.FileFilter;
 
+import org.codehaus.plexus.util.DirectoryScanner;
 import org.testng.annotations.Test;
+
+import edu.emory.mathcs.backport.java.util.Collections;
 
 public class DirectoryIteratorTest {
 	@Test
@@ -14,6 +17,19 @@ public class DirectoryIteratorTest {
 		FileFilter fileFilter = new FolderFilter();
 		File[] listFiles = folder.listFiles(fileFilter);
 		assertThat(listFiles).hasSize(2).containsOnly(new File("./src"), new File("./target"));
+	}
+	
+	@Test
+	public void secondTest() {
+		DirectoryScanner dc = new DirectoryScanner();
+		dc.addDefaultExcludes();
+		dc.setBasedir(new File("."));
+		dc.setIncludes(new String[]{"*"});
+		dc.scan();
+		String[] includedDirectories = dc.getIncludedDirectories();
+		for (int i = 0; i < includedDirectories.length; i++) {
+			System.out.println("Folder: " + includedDirectories[i]);
+		}
 	}
 
 }
